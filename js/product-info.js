f63965dc8160e18c0c95084c80f1fbee2e5e7023
AndreaBustos99 <andi.bustos99@gmail.com>
2022-08-21 22:52:39 -0300

    Agregado de productos (autos) desde un json

---
+++ js/product-info.js
@@ -0,0 +1,28 @@
+async function getData(){
+    let respuesta = await fetch('https://japceibal.github.io/emercado-api/cats_products/101.json');
+    let informacion = await respuesta.json();
+    return informacion;
+}
+
+document.addEventListener('DOMContentLoaded', async function(){
+    let datos= await getData();
+    let autoLista = datos.products
+    productos.innerHTML= " "
+    autoLista.forEach(auto => {
+        productos.innerHTML+= `
+        <div class="card" style="width: 18rem; margin: 20px;">
+        <img src=${auto.image} class="card-img-top" alt="...">
+        <div class="card-body"><h5 class="card-title">${auto.name}</h5>
+        <p class="card-text">${auto.description}</p>
+        </div><ul class="list-group list-group-flush">
+        <li class="list-group-item">${auto.currency} ${auto.cost}</li>
+        <li class="list-group-item">${auto.soldCount}</li>
+        </ul>
+        </div>
+        `;
+    });
+});
+
+/* function mostrar(auto){
+    console.log(auto.products)
+} */
